30cc5a0b903a3b50a8ff764d1e03642ba9919083
Mateusz Dyderski <dydek.2002@wp.pl>
2022-06-09 22:22:39 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+{
+
+
+    const calculateResult = (amount, currency) => {
+
+        const rateUSD = 4.3199;
+        const rateEUR = 4.5976;
+        const rateGBP = 5.4060;
+        const rateCZK = 0.1852;
+        const rateNOK = 0.4511;
+
+        switch (currency) {
+            case "USD":
+                return amount / rateUSD;
+            case "EUR":
+                return amount / rateEUR;
+
+            case "GBP":
+                 return amount / rateGBP;
+
+            case "CZK":
+                return amount / rateCZK;
+
+            case "NOK":
+                return amount / rateNOK;
+        }
+    };
+
+    const updateResultText = (amount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong> ${result.toFixed(2)} ${currency} </strong>`;
+
+    }
+
+    const onFormSubmit = (event) => {
+            event.preventDefault();
+        
+            
+            const amountElement = document.querySelector(".js-amount");
+            const currencyElement = document.querySelector(".js-currency");
+        
+            const amount = +amountElement.value;
+            const currency = currencyElement.value;
+        
+            let result = calculateResult(amount, currency);
+
+            updateResultText(amount, result, currency);
+         
+        };
+
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
